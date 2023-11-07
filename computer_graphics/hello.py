4763eb15bf6badab756a67ea840b886dcc514a8a
Artemy Pozdnyakov <aapozdnyakove@gmail.com>
2023-11-07 20:45:19 +0300

    NOT BLACK
    Signed-off-by: Artemy Pozdnyakov <aapozdnyakove@gmail.com>

--- computer_graphics/hello.py
+++ computer_graphics/hello.py
@@ -1,6 +1,11 @@
+import math
 import numpy as np
+from pyrr import Matrix44
+import moderngl
 
 import computer_graphics.base as base
+
+from PIL import Image
 
 
 class Hello(base.WindowBase):
@@ -11,38 +16,87 @@ class Hello(base.WindowBase):
 
         self.prog = self.ctx.program(
             vertex_shader='''
-                #version 330
+                #version 330 core
 
-                in vec2 in_vert;
+                in vec3 aVertexPosition;
 
-                void main() {
-                    gl_Position = vec4(in_vert, 0.0, 1.0);
+                uniform mat4 mvp;
+
+                void main(void) {
+                    gl_Position = mvp * vec4(aVertexPosition, 1.0);
                 }
             ''',
             fragment_shader='''
-                #version 330
-
-                out vec4 f_color;
-
-                void main() {
-                    f_color = vec4(0.2, 0.4, 0.7, 1.0);
+                #version 330 core
+                
+                void main(void) {
+                    gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
                 }
             ''',
         )
 
         vertices = np.array([
-            0.0, 0.8,
-            -0.6, -0.8,
-            0.6, -0.8,
+            -1.0, -1.0, 0.0,
+            -1.0, 1.0, 0.0,
+            1.0, 1.0, 0.0,
+            -1.0, -1.0, 0.0,
+            -1.0, 1.0, 0.0,
+            1.0, 1.0, 0.0,
+            -1.0, -1.0, 0.0,
+            -1.0, 1.0, 0.0,
+            1.0, 1.0, 0.0,
+            -1.0, -1.0, 0.0,
+            -1.0, 1.0, 0.0,
+            1.0, 1.0, 0.0,
+            -1.0, -1.0, 0.0,
+            -1.0, 1.0, 0.0,
+            1.0, 1.0, 0.0,
+            -1.0, -1.0, 0.0,
+            -1.0, 1.0, 0.0,
+            1.0, 1.0, 0.0,
+            -1.0, -1.0, 0.0,
+            -1.0, 1.0, 0.0,
+            1.0, 1.0, 0.0,
+            # 1.0, -1.0, 0.0,
+            # -1.0, -1.0, 0.0,
+        ], dtype='f4')
+
+        self.vbo = self.ctx.buffer(vertices.tobytes())
+        self.vao = self.ctx.vertex_array(self.prog, [
+            (self.vbo, '3f', 'aVertexPosition'),
         ])
 
-        self.vbo = self.ctx.buffer(vertices.astype('f4').tobytes())
-        self.vao = self.ctx.vertex_array(self.prog, self.vbo, 'in_vert')
+        # self.fbo = self.ctx.framebuffer(color_attachments=[self.ctx.texture((512, 512), 4)])
+
 
     def render(self, time: float, frame_time: float):
-        self.ctx.screen.clear(color=(1.0, 1.0, 1.0))
-        self.vao.render()
+        # self.fbo.use()
+        self.ctx.enable(moderngl.DEPTH_TEST)
+        # self.ctx.clear(color=(0.0, 0.0, 0.0, 1.0))
+        self.ctx.clear(0.0, 0.0, 0.0, 0.0)
 
+        fieldOfView = (90 * math.pi) / 180 # in radians
+        aspect = self.window_size[0] / self.window_size[1]
+        zNear = 0.1
+        zFar = 100.0
+
+        perspective = Matrix44.perspective_projection(fieldOfView, 
+                                                            aspect, zNear, zFar, dtype='f4')
+        lookat = Matrix44.look_at(
+            (4, 3, 2),
+            (0.0, 0.0, 0.0),
+            (0.0, 1.0, 0.0),
+            dtype='f4'
+        )
+
+        mvp = perspective * lookat
+        model_view_matrix = Matrix44()
+        model_view_matrix.from_translation(np.array([0.0, 0.0, 0.0], dtype='f4'))
+        self.prog["mvp"].write(mvp.tobytes())
+
+        # self.prog['model'].write(Matrix44.from_eulers((0.0, 0.1, 0.0), dtype='f4'))
+        self.vao.render(mode=moderngl.TRIANGLES)
+        
 
 if __name__ == '__main__':
     Hello.run()
