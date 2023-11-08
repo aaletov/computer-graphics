7786571f47ba3b4e0bf7b92a9eee835b2d644c29
Artemy Pozdnyakov <aapozdnyakove@gmail.com>
2023-11-08 06:31:42 +0300

    Minor refactoring
    Signed-off-by: Artemy Pozdnyakov <aapozdnyakove@gmail.com>

--- computer_graphics/hello.py
+++ computer_graphics/hello.py
@@ -4,9 +4,34 @@ from pyrr import Matrix44
 import moderngl
 
 from .base import WindowBase
-from .polyhedron import createDodecahedron, createCube
+from .polyhedron import createDodecahedron, createCube, Polyhedron
 
 from PIL import Image
+
+class CgSolid:
+    def __init__(self, ctx: moderngl.Context, program: moderngl.Program, polyhedron: Polyhedron) -> None:
+        self.ctx = ctx
+        self.prog = program
+        raw_vertices = []
+        triangles = polyhedron.get_cover_triangles()
+        for triangle in triangles:
+            raw_vertices += [
+                *triangle[0],
+                *triangle[1],
+                *triangle[2],
+            ]
+        vertices = np.array(raw_vertices, dtype='f4')
+
+        self.vbo = self.ctx.buffer(vertices.tobytes())
+        self.vao = self.ctx.vertex_array(self.prog, [
+            (self.vbo, '3f', 'aVertexPosition'),
+        ])
+
+    def render(self, x: float) -> None:
+        model_matrix = Matrix44.from_translation(np.array([x, 0.0, 0.0]), dtype='f4')
+        self.prog["model"].write(model_matrix.tobytes())
+        self.vao.render(mode=moderngl.TRIANGLES)
+
 
 class Hello(WindowBase):
     title = 'Hello Program'
@@ -20,10 +45,11 @@ class Hello(WindowBase):
 
                 in vec3 aVertexPosition;
 
-                uniform mat4 mvp;
+                uniform mat4 model;
+                uniform mat4 vp;
 
                 void main(void) {
-                    gl_Position = mvp * vec4(aVertexPosition, 1.0);
+                    gl_Position = model * vp * vec4(aVertexPosition, 1.0);
                 }
             ''',
             fragment_shader='''
@@ -35,27 +61,10 @@ class Hello(WindowBase):
             ''',
         )
 
-
-        dodecahedron = createCube()
-        # line = dodecahedron.get_cover_line()
-        raw_vertices = []
-        # for point in line:
-        #     for c in point:
-        #         raw_vertices.append(c)
-        triangles = dodecahedron.get_cover_triangles()
-        for triangle in triangles:
-            raw_vertices += [
-                *triangle[0],
-                *triangle[1],
-                *triangle[2],
-            ]
-        print(raw_vertices)
-        vertices = np.array(raw_vertices, dtype='f4')
-
-        self.vbo = self.ctx.buffer(vertices.tobytes())
-        self.vao = self.ctx.vertex_array(self.prog, [
-            (self.vbo, '3f', 'aVertexPosition'),
-        ])
+        self.solids = [
+            CgSolid(self.ctx, self.prog, createCube()),
+            CgSolid(self.ctx, self.prog, createDodecahedron()),
+        ]
 
         # self.fbo = self.ctx.framebuffer(color_attachments=[self.ctx.texture((512, 512), 4)])
 
@@ -74,19 +83,18 @@ class Hello(WindowBase):
         perspective = Matrix44.perspective_projection(fieldOfView, 
                                                             aspect, zNear, zFar, dtype='f4')
         lookat = Matrix44.look_at(
-            (20, 20, 100),
+            (20, 20, 150),
             (0.0, 0.0, 0.0),
             (0.0, 1.0, 0.0),
             dtype='f4'
         )
 
-        mvp = perspective * lookat
-        model_view_matrix = Matrix44()
-        model_view_matrix.from_translation(np.array([0.0, 0.0, 0.0], dtype='f4'))
-        self.prog["mvp"].write(mvp.tobytes())
+        vp = perspective * lookat
+        self.prog["vp"].write(vp.tobytes())
 
         # self.prog['model'].write(Matrix44.from_eulers((0.0, 0.1, 0.0), dtype='f4'))
-        self.vao.render(mode=moderngl.TRIANGLES)
+        self.solids[0].render(0.0)
+        self.solids[1].render(0.5)
         
 
 if __name__ == '__main__':
